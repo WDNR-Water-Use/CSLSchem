3a33be94c00b8721aa0f40dfbf8d4e8cb0cbf4ee
Carolyn Voter <cvoter@wisc.edu>
2020-05-14 09:51:48 -0500

    Add data from Lillie and Mason 1983 survey of WI lake water chemistry for comparison

---
+++ R/lillie_mason_lakes.R
@@ -0,0 +1,22 @@
+#' Data from Wisconsin Lakes survey by Lillie and Mason
+#'
+#' @references Lillie, R.A. and J.W. Mason 1983. Limnological Characteristics of Wisconsin Lakes. Technical Bulletin No. 138. Wisconsin Department of Natural Resources. Madison, Wisconsin.
+#'
+#' @docType data
+#'
+#' @usage data(lillie_mason_lakes)
+#'
+#' @format A data frame with the following columns:
+#' \describe{
+#'  \item{location}{WI, central, or seepage}
+#'  \item{description}{parameter, corresponds to dnr_description where possible}
+#'  \item{units}{units of measured value}
+#'  \item{n}{number of lakes sampled}
+#'  \item{mean}{mean value}
+#'  \item{std}{standard deviation of values}
+#'  \item{median}{median value}
+#'  \item{min}{minimum value}
+#'  \item{max}{maximum value}
+#'  }
+#'
+"lillie_mason_lakes"
